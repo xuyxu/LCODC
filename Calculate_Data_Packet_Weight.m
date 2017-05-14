2485b8c5a22f13018df32661c0f7e5c4d143b233
Yixuan Xu <yixuan_xu@csu.edu.cn>
2017-05-14 13:31:15 +0800

    Update Calculate_Data_Packet_Weight.m

--- Calculate_Data_Packet_Weight.m
+++ Calculate_Data_Packet_Weight.m
@@ -1,10 +1,11 @@
 function [Weight] = Calculate_Data_Packet_Weight(Buffer, Current_Time_Slice)
 
 k = 1000;
-% 预分配内存
+% Pre-allocate memory
 Weight = zeros(size(Buffer,1),1);
 
-% 计算数据包权值的公式（针对刚产生的数据包，权值为无限大，即一定会放在内存中）
+% Calculate weights of data packets
+% Weights of Newly generated data packets are Inf, i.e. they will not be discarded when they are generated
 Weight(:,1) = (1 ./ Buffer(:,3)) + k * (1./ (Current_Time_Slice - Buffer(:,4)));
 
 % Weight = rand(size(Buffer,1),1);
