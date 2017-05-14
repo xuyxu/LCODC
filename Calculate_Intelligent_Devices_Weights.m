05b700bce56f97e8d0bb4b4f433ecc90b6639162
Yixuan Xu <yixuan_xu@csu.edu.cn>
2017-05-14 13:34:38 +0800

    Update Calculate_Intelligent_Devices_Weights.m

--- Calculate_Intelligent_Devices_Weights.m
+++ Calculate_Intelligent_Devices_Weights.m
@@ -1,20 +1,20 @@
 tic;
-Device_Weight = zeros(size(new_Dataset,1),1); % 预分配内存
+Device_Weight = zeros(size(new_Dataset,1),1); % Pre-allocate memory
 
-Maximum_Distance = 5; % 数据中心的接收半径为500m
-t_weight = 0; % 用于记录智能设备权值的中间变量
+Maximum_Distance = 1; % reception radius of data centers
+t_weight = 0;
 
 for i = 1 : size(new_Dataset,1)
     
     [x_temp,y_temp] = GridIndex_Calculate(new_Dataset{i,1}.Location);
     
     for j = 1 : size(centroids,1)
-        temp = abs(x_temp-centroids(j,1)) + abs(y_temp-centroids(j,2)); % 计算与第j个数据中心的曼哈顿距离
-        t_weight = t_weight + size(find(temp <= Maximum_Distance),1); % 统计所有小于接收半径的点个数
+        temp = abs(x_temp-centroids(j,1)) + abs(y_temp-centroids(j,2)); % calculate its Manhattan distance to the ith data center
+        t_weight = t_weight + size(find(temp <= Maximum_Distance),1);
     end
     
-    Device_Weight(i,1) = t_weight; % 赋值
-    t_weight = 0; % 清零
+    Device_Weight(i,1) = t_weight;
+    t_weight = 0;
         
 end
 toc;
