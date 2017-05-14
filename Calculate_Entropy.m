8603a0ac47e7edfc96c6d44b658b2abe3f350f0f
Yixuan Xu <yixuan_xu@csu.edu.cn>
2017-05-14 13:32:05 +0800

    Update Calculate_Entropy.m

--- Calculate_Entropy.m
+++ Calculate_Entropy.m
@@ -1,4 +1,4 @@
-% 预分配内存
+% Pre-allocate memory
 for i = 1 : 2050
     for j = 1 : 3453
         result{i,j} = zeros(2,1);
@@ -6,7 +6,7 @@ for i = 1 : 2050
 end
 fprintf('pre-allocate memory finished!\n');
 
-% 为每个网格构建记录列表
+% Construct record for each grid
 for i = 1 : size(new_Dataset,1)
     for j = 1 : size(new_Dataset{i,1}.Location,1)
       [x, y] = GridIndex_Calculate(new_Dataset{i,1}.Location(j,:));
